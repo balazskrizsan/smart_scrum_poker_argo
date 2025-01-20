cd94a9034f1cd1593f62bc178effdba69a4e9ac7
Balazs Krizsan <krizsan.balazs@gmail.com>
2025-01-21 00:00:00 +0100

    cert-install-minikube.sh using dev_keys folder

--- src/cert-install-minikube.sh
+++ src/cert-install-minikube.sh
@@ -1,4 +1,4 @@
 # https://minikube.sigs.k8s.io/docs/tutorials/custom_cert_ingress/
 kubectl -n kube-system create secret tls mkcert \
-    --key keystore/privkey.pem \
-    --cert keystore/fullchain.pem
+    --key dev_keys/privkey.pem \
+    --cert dev_keys/fullchain.pem
