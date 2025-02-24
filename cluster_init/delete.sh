36357be8b6dd2e1a71d9bda40d85a0a1a6d798a7
Balazs Krizsan <krizsan.balazs@gmail.com>
2025-02-24 18:50:34 +0100

    cluster_init delete.sh fix

--- cluster_init/delete.sh
+++ cluster_init/delete.sh
@@ -1,7 +1,3 @@
-kubectl delete namespace smart-scrum-poker-prod
-kubectl delete namespace smart-scrum-poker-uat
-kubectl delete namespace smart-scrum-poker-elk-common
-
 kubectl delete -f ./set-google-service-account.yaml
 kubectl delete -f ./cluster-extensions/argocd-ingress.yaml
 kubectl delete -f ./cluster-extensions/minikube-dashboard-ingress.yaml
@@ -9,3 +5,7 @@ kubectl delete -f ./cluster-extensions/minikube-dashboard-ingress.yaml
 kubectl delete -f ./argo_apps/smart-scrum-poker-prod-app.yaml
 kubectl delete -f ./argo_apps/smart-scrum-poker-uat-app.yaml
 kubectl delete -f ./argo_apps/smart-scrum-poker-elk-common-app.yaml
+
+kubectl delete namespace smart-scrum-poker-prod
+kubectl delete namespace smart-scrum-poker-uat
+kubectl delete namespace smart-scrum-poker-elk-common
