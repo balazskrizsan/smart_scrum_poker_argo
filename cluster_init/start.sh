8b4a49e7de2c832d4f34ef05dba2687c2645a1ca
Balazs Krizsan <krizsan.balazs@gmail.com>
2025-02-24 17:19:05 +0100

    cluster_init/start.sh with uat

--- cluster_init/start.sh
+++ cluster_init/start.sh
@@ -1,5 +1,7 @@
 kubectl create namespace smart-scrum-poker-prod
+kubectl create namespace smart-scrum-poker-uat
 kubectl create namespace smart-scrum-poker-elk-common
 kubectl apply -f ./set-google-service-account.yaml
 kubectl apply -f ./argo_apps/smart-scrum-poker-prod-app.yaml
+kubectl apply -f ./argo_apps/smart-scrum-poker-uat-app.yaml
 kubectl apply -f ./argo_apps/smart-scrum-poker-elk-common-app.yaml
