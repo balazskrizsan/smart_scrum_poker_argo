e84c6f17e335582c287cffab39156cf34dad3d19
Balazs Krizsan <krizsan.balazs@gmail.com>
2025-02-24 17:59:35 +0100

    cluster_init refact

--- cluster_init/delete.sh
+++ cluster_init/delete.sh
@@ -1,5 +1,11 @@
-kubectl delete -f ./set-google-service-account.yaml
-kubectl delete -f ./argo_apps/smart-scrum-poker-prod-app.yaml
-kubectl delete -f ./argo_apps/smart-scrum-poker-elk-common-app.yaml
 kubectl delete namespace smart-scrum-poker-prod
+kubectl delete namespace smart-scrum-poker-uat
 kubectl delete namespace smart-scrum-poker-elk-common
+
+kubectl delete -f ./set-google-service-account.yaml
+kubectl delete -f ./cluster-extensions/argocd-ingress.yaml
+kubectl delete -f ./cluster-extensions/minikube-dashboard-ingress.yaml
+
+kubectl delete -f ./argo_apps/smart-scrum-poker-prod-app.yaml
+kubectl delete -f ./argo_apps/smart-scrum-poker-uat-app.yaml
+kubectl delete -f ./argo_apps/smart-scrum-poker-elk-common-app.yaml
--- cluster_init/start.sh
+++ cluster_init/start.sh
@@ -1,7 +1,11 @@
 kubectl create namespace smart-scrum-poker-prod
 kubectl create namespace smart-scrum-poker-uat
 kubectl create namespace smart-scrum-poker-elk-common
+
 kubectl apply -f ./set-google-service-account.yaml
+kubectl apply -f ./cluster-extensions/argocd-ingress.yaml
+kubectl apply -f ./cluster-extensions/minikube-dashboard-ingress.yaml
+
 kubectl apply -f ./argo_apps/smart-scrum-poker-prod-app.yaml
 kubectl apply -f ./argo_apps/smart-scrum-poker-uat-app.yaml
 kubectl apply -f ./argo_apps/smart-scrum-poker-elk-common-app.yaml
