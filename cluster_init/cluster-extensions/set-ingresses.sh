629dc4d9a9978133058bdd9e8fb8212453bf1f87
Balazs Krizsan <krizsan.balazs@gmail.com>
2025-02-27 13:05:46 +0100

    set-ingresses path updates

--- cluster_init/cluster-extensions/set-ingresses.sh
+++ cluster_init/cluster-extensions/set-ingresses.sh
@@ -1,5 +1,5 @@
-kubectl delete -n argocd -f argocd-ingress.yaml --ignore-not-found
-kubectl apply  -n argocd -f argocd-ingress.yaml
+kubectl delete -n argocd -f ./cluster-extensions/argocd-ingress.yaml --ignore-not-found
+kubectl apply  -n argocd -f ./cluster-extensions/argocd-ingress.yaml
 
-kubectl delete -n kubernetes-dashboard -f minikube-dashboard-ingress.yaml --ignore-not-found
-kubectl apply  -n kubernetes-dashboard -f minikube-dashboard-ingress.yaml
+kubectl delete -n kubernetes-dashboard -f ./cluster-extensions/minikube-dashboard-ingress.yaml --ignore-not-found
+kubectl apply  -n kubernetes-dashboard -f ./cluster-extensions/minikube-dashboard-ingress.yaml
