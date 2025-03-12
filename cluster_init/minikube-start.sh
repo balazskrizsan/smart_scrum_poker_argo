6b2d1a61f278f2673a0429ef5066cd8888b0814c
Balazs Krizsan <krizsan.balazs@gmail.com>
2025-03-12 20:30:41 +0100

    minikube-start.sh with addons

--- cluster_init/minikube-start.sh
+++ cluster_init/minikube-start.sh
@@ -5,6 +5,6 @@ minikube start \
   --apiserver-port=8443 \
   --cpus=7 \
   --memory=15360 \
+  --addons=ingress,dashboard,metrics-server \
   --mount --mount-string="/minikube_backup/ssp/prod_psql_data:/mnt/ssp/prod_psql_data" \
   --mount --mount-string="/minikube_backup/ssp/uat_psql_data:/mnt/ssp/uat_psql_data"
-
