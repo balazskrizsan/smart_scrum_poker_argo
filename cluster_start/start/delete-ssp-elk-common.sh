63fcc15445ac4b947fba242c4f395391db80f81a
Balazs Krizsan <krizsan.balazs@gmail.com>
2025-03-14 22:51:22 +0100

    cluster_start\start delete fixes

--- cluster_start/start/delete-ssp-elk-common.sh
+++ cluster_start/start/delete-ssp-elk-common.sh
@@ -1,3 +1,3 @@
-kubectl delete -f ./argo_apps/smart-scrum-poker-elk-common-app.yaml
+kubectl delete -f ./start/argo-apps/smart-scrum-poker-elk-common-app.yaml
 
 kubectl delete namespace smart-scrum-poker-elk-common
