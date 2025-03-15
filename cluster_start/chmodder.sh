1bc0022f763f70a3c078ddf8dde2e7106f13e5da
Balazs Krizsan <krizsan.balazs@gmail.com>
2025-03-15 17:40:43 +0100

    chmodder.sh update

--- cluster_start/chmodder.sh
+++ cluster_start/chmodder.sh
@@ -1,4 +1,8 @@
 chmod +x start-all.sh
+chmod +x start-apps.sh
+chmod +x start-elk.sh
 chmod +x delete-all.sh
+chmod +x delete-apps.sh
+chmod +x delete-elk.sh
 
 find ./start -type f -name "*.sh" -exec chmod +x {} \;
