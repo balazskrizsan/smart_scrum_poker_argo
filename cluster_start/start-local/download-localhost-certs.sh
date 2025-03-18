0aa1b1fa7fa0a6c4e7d42f8b5883f66a606c79dd
Balazs Krizsan <krizsan.balazs@gmail.com>
2025-03-18 01:47:01 +0100

    download-localhost-certs.sh v2->v3

--- cluster_start/start-local/download-localhost-certs.sh
+++ cluster_start/start-local/download-localhost-certs.sh
@@ -1,6 +1,6 @@
 mkdir -p "./start-local/certs"
 
 [ -f "./start-local/certs/certificate.crt" ] || curl -o "./start-local/certs/certificate.crt" \
-  "https://smart-scrum-poker-secrets.s3.us-east-1.amazonaws.com/cert--*.localhost.krizsanbalazs.com/v2/certificate.crt"
+  "https://smart-scrum-poker-secrets.s3.us-east-1.amazonaws.com/cert--*.localhost.krizsanbalazs.com/v3/certificate.crt"
 [ -f "./start-local/certs/private.key" ] || curl -o "./start-local/certs/private.key" \
-  "https://smart-scrum-poker-secrets.s3.us-east-1.amazonaws.com/cert--*.localhost.krizsanbalazs.com/v2/private-decrypted.key"
+  "https://smart-scrum-poker-secrets.s3.us-east-1.amazonaws.com/cert--*.localhost.krizsanbalazs.com/v3/private-decrypted.key"
