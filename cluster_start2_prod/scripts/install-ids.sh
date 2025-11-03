2db1544c2c5a2347d9b59b150c5a35a0642fbe94
Balazs Krizsan <krizsan.balazs@gmail.com>
2025-11-03 19:11:20 +0100

    cluster_start2_prod\scripts ids install fixes + uninstall

--- cluster_start2_prod/scripts/install-ids.sh
+++ cluster_start2_prod/scripts/install-ids.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-git clone https://github.com/wso2/kubernetes-is.git
+rm -rf repos && mkdir -p repos
 
-cp ./scripts/install-ids-helm-values.yaml ./scripts/kubernetes-is/values.yaml
+git clone https://github.com/wso2/kubernetes-is.git ./repos/wso2-ids
 
-helm install "ids--ssp--prod-release" -n "ids--ssp--prod" . \
+cp ./scripts/install-ids-helm-values.yaml ./repos/wso2-ids/values.yaml
+
+helm install "ids--prod" -n "ids--ssp--prod" ./repos/wso2-ids \
   --set deployment.image.registry="wso2" \
   --set deployment.image.repository="wso2is" \
   --set deployment.image.tag="7.1.0" \
